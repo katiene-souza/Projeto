79809d9a3943ef985fb0ccad36dde2b41cc2d642
katiene-souza <85809975+katiene-souza@users.noreply.github.com>
2023-01-11 22:18:34 -0300

    Add files via upload

---
+++ projeto/cadastro.js
@@ -0,0 +1,67 @@
+const form = document.getElementById('cadastro')
+const nome = document.querySelector('#nome')
+const email = document.querySelector('#email')
+const senha = document.querySelector('#senha')
+
+form.addEventListener ('submit', (event) => {
+    event.preventDefault()
+    if(nome.value === '' && email.value === '' && senha.value === ''){
+        alert('você precisa preencher todos os campos')
+        nome.setAttribute('style', 'border-color: red')
+        email.setAttribute('style', 'border-color: red')
+        senha.setAttribute('style', 'border-color: red')
+     } else if (validarNome(nome.value) === true && validarEmail(email.value) === true && validarSenha(senha.value) === true) {
+        let listaUsuario = JSON.parse(localStorage.getItem('listaUsuario') || '[]')
+         listaUsuario.push({
+            nome: nome.value,
+            email: email.value,
+            senha: senha.value
+        })
+         localStorage.setItem('listaUsuario', JSON.stringify(listaUsuario))
+         window.location.href = ('http://127.0.0.1:5500/modulo1/projeto/login.html')
+     }
+    else {
+        alert('email ou senha incorretos!')
+    }
+})
+
+nome.addEventListener('keyup', () => {
+    if(validarNome(nome.value) !== true) {
+    nome.setAttribute('style', 'border-color: red')
+    } else {
+    nome.setAttribute('style', 'border-color: green')
+    }
+})
+
+email.addEventListener('keyup', () => {
+    if(validarEmail(email.value) !== true) {
+    email.setAttribute('style', 'border-color: red')
+    } else {
+    email.setAttribute('style', 'border-color: green')
+    }
+})
+
+senha.addEventListener('keyup', () => {
+    if(validarSenha (senha.value) !== true) {
+        senha.setAttribute('style', 'border-color: red')
+    } else {
+        senha.setAttribute('style', 'border-color: green')
+    }
+    })
+
+
+function validarNome(nome) { 
+    let nomePadrao = /[A-z][ ][A-z]/; 
+    return nomePadrao.test(nome); 
+} 
+
+function validarEmail(email) {
+    let emailPadrao = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+    return emailPadrao.test(email);
+}
+
+function validarSenha(senha) {
+    let senhaPadrao = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return senhaPadrao.test(senha);
+}
+
---
+++ projeto/login.js
@@ -0,0 +1,75 @@
+const form = document.getElementById('login')
+const email = document.querySelector('#email')
+const senha = document.querySelector('#senha')
+
+form.addEventListener ('submit', (event) => {
+    event.preventDefault()
+    if(email.value === '' && senha.value === ''){
+        alert('você precisa preencher todos os campos')
+        email.setAttribute('style', 'border-color: red')
+        senha.setAttribute('style', 'border-color: red')
+       
+    } else if (validarEmail(email.value) === true && validarSenha(senha.value) === true) {
+
+        let listaUsuario =  [ ]
+
+        let usuarioValidado = {
+            nome: '',
+            email: '',
+            senha: ''
+        }
+        listaUsuario = JSON.parse(localStorage.getItem('listaUsuario'))
+        
+        listaUsuario.forEach ((item) => {
+            if (email.value == item.email && senha.value == item.senha) {
+                usuarioValidado =  {
+                    nome: item.nome,
+                    email: item.email,
+                    senha: item.senha 
+                }
+            }
+            
+        })
+        if(email.value == usuarioValidado.email && senha.value == usuarioValidado.senha) {
+            window.location.href = ('http://127.0.0.1:5500/modulo1/projeto/gerenciador.html')
+        } else {
+            alert('email ou senha incorretos!')
+            email.setAttribute('style', 'border-color: red')
+            senha.setAttribute('style', 'border-color: red')
+        }
+        
+    }
+    else {
+        alert('email ou senha incorretos!')
+        email.setAttribute('style', 'border-color: red')
+        senha.setAttribute('style', 'border-color: red')
+    }
+})
+
+email.addEventListener('keyup', () => {
+    if(validarEmail(email.value) !== true) {
+    email.setAttribute('style', 'border-color: red')
+    } else {
+    email.setAttribute('style', 'border-color: green')
+    }
+})
+
+senha.addEventListener('keyup', () => {
+    if(validarSenha (senha.value) !== true) {
+        senha.setAttribute('style', 'border-color: red')
+    } else {
+        senha.setAttribute('style', 'border-color: green')
+    }
+    })
+ 
+function validarEmail(email) {
+    let emailPadrao =
+    /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+    return emailPadrao.test(email);
+}
+
+function validarSenha(senha) {
+    let senhaPadrao =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return senhaPadrao.test(senha);
+}
